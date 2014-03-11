969a78cdfeb9f64cbc2c03ca91b007d7eed457c4
Richeng Jin <jinricheng123@gmail.com>
2014-03-11 12:57:24 +0100

    Primera session

---
+++ sobres/isobres/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Sobre(models.Model):
+	date = models.DateTimeField()
+	amount = models.IntegerField()
+	concept = models.TextField(max_length=100)
